c89284afbeb3d7a15251d20dc53152c6a5b93919
unknown <pm161118@outlook.com>
2022-09-16 14:45:15 +0100

    Initial Commit

--- airbnb/src/screens/Home/index.js
+++ airbnb/src/screens/Home/index.js
@@ -5,23 +5,23 @@ import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const HomeScreen = props => {
   return (
-      <View>
-          
+    <View>
       <ImageBackground
         source={require('../../../assets/images/wallpaper.jpg')}
-              style={styles.image}>
-              <Pressable
+        style={styles.image}>
+        <Pressable
           style={styles.searchButton}
-          onPress={() => console.log('Search Btn Clicked')}
-              ><Fontisto name='search' size={25} color={'#f15454'} /><Text style={styles.buttonText}> Where are you going?</Text>
-                  </Pressable>
+          onPress={() => console.log('Search Btn Clicked')}>
+          <Fontisto name="search" size={25} color={'#f15454'} />
+          <Text style={styles.buttonText}> Where are you going?</Text>
+        </Pressable>
         <Text style={styles.title}>Go Near</Text>
 
         <Pressable
           style={styles.button}
-          onPress={() => console.log('Exp Btn Clicked')}
-              ><Text style={styles.buttonText}>Explore nearby stays</Text>
-                  </Pressable>
+          onPress={() => console.log('Exp Btn Clicked')}>
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
       </ImageBackground>
     </View>
   );
