fd60369e35da3eec35f239b35dc9ad2ca21e9e97
unknown <pm161118@outlook.com>
2022-09-19 21:15:09 +0100

    Added amplify API and populated stays

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "rna": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
